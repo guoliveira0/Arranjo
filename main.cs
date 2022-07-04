8bdd1350f4dedee16be2e8be650431855ba8d265
Luiz Gustavo <106283091+guoliveira0@users.noreply.github.com>
2022-07-04 19:57:46 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //Int32[] arr1 = new Int32[5];
+
+    /*Console.WriteLine(arr1[0]);
+    Console.WriteLine(arr1[1]);
+    Console.WriteLine(arr1[2]);
+    Console.WriteLine(arr1[3]);
+    Console.WriteLine(arr1[4]);*/
+    
+    /*Console.WriteLine([0]);
+    Console.WriteLine([1]);
+    Console.WriteLine([2]);
+    Console.WriteLine([3]);
+    Console.WriteLine([4]);*/
+
+     //Int32[] arr1 = new Int32[] {99, 88, 77, 66};
+    //Boolean[] arr1 = new Boolean[] {true, false, true, false};
+    String[]arr1 
+  }
+}
