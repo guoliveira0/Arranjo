11931c3e179a807f6cac887e0c9632c6e53aa6d4
Luiz Gustavo <106283091+guoliveira0@users.noreply.github.com>
2022-07-04 20:20:03 +0000

    Versão 2 de arranjos discente

--- main.cs
+++ main.cs
@@ -3,15 +3,17 @@ using System;
 class Program {
   public static void Main (string[] args) {
   Discente[] arr1 = new Discente[3];
-  Discente d1;
+  /*Discente d1;
   d1 = new Discente(789, "Ana", 16);
-  arr1[0] = d1;
+  arr1[0] = d1;*\
     
-  Console.WriteLine(arr1[0]);
     
   /*Discente d2 = new Discente();
   d2.Matricula = 34;
   d2.Nome = "Esther Fernandes";
   d2.Idade = 23;*/
+
+  arr1[0] = new Discente(789, "Ana", 16); 
+  Console.WriteLine(arr1[0]);
   }
 }
