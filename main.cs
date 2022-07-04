358438a552bdab492e945ac48b85821dfc1a7c07
Luiz Gustavo <106283091+guoliveira0@users.noreply.github.com>
2022-07-04 20:32:42 +0000

    Versão 3 arranjos, com for

--- main.cs
+++ main.cs
@@ -2,7 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-  Discente[] arr1 = new Discente[3];
+  Discente[] arr1 = new Discente[4];
   /*Discente d1;
   d1 = new Discente(789, "Ana", 16);
   arr1[0] = d1;*\
@@ -12,8 +12,12 @@ class Program {
   d2.Matricula = 34;
   d2.Nome = "Esther Fernandes";
   d2.Idade = 23;*/
-
-  arr1[0] = new Discente(789, "Ana", 16); 
-  Console.WriteLine(arr1[0]);
+  arr1[0] = new Discente(90, "Luiz Gustavo", 56);
+  arr1[1] = new Discente(45, "Luiz Gustavo", 23);
+  arr1[2] = new Discente(89, "Luiz Gustavo", 56);
+  arr1[3] = new Discente(546, "Luiz Gustavo", 78);
+  for(int i = 0; i< arr1.Length; i++){
+    Console.WriteLine(arr1[i].Idade);
+  }
   }
 }
