229e1935bf9b5eb7d36f06fcb4e627e623e8057d
Luiz Gustavo <106283091+guoliveira0@users.noreply.github.com>
2022-07-06 11:59:04 +0000

    exemplo de list, indexof

--- main.cs
+++ main.cs
@@ -5,6 +5,8 @@ class Program {
   public static void Main (string[] args) {
   List <string> nomes = new List<string>();
   nomes.Add("Luiz Gustavo");
+  Boolean contem = nomes.Contains("Luiz Gustavo");
+  Console.WriteLine("Luiz Gustavo encontrado: " + contem);
   foreach(string nome in nomes){
     Console.WriteLine(nome);
   }
